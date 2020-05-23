8215a913ba9ad5132e9ad2f8d13d9d950efd3248
Bl4Cc4t <kuron3ko@cock.li>
2020-05-23 14:05:00 +0200

    [0.0.12] forgot to update the version number

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GoodTwitter 2 - Electric Boogaloo
-// @version       0.0.11
+// @version       0.0.12
 // @description   A try to make Twitter look good again
 // @author        schwarzkatz
 // @match         https://twitter.com/*
