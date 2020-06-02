75cb1550e23ed4e98790c45c979c80c6ddfb0508
Bl4Cc4t <kuron3ko@cock.li>
2020-06-02 13:44:14 +0200

    [0.0.17] frick, I keep forgetting to change the version numbering :V

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GoodTwitter 2 - Electric Boogaloo
-// @version       0.0.16
+// @version       0.0.17
 // @description   A try to make Twitter look good again
 // @author        schwarzkatz
 // @match         https://twitter.com/*
