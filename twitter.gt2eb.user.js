142df1fa6503a3e0b9ab31387aa397dd638fb659
Bl4Cc4t <kuron3ko@cock.li>
2020-06-23 01:28:49 +0200

    display profile infos for blocked accounts (fixes #133)

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -182,7 +182,10 @@
       moon: `<g><path d="M 13.277344 24 C 16.976562 24 20.355469 22.316406 22.597656 19.554688 C 22.929688 19.148438 22.566406 18.550781 22.054688 18.648438 C 16.234375 19.757812 10.886719 15.292969 10.886719 9.417969 C 10.886719 6.03125 12.699219 2.917969 15.644531 1.242188 C 16.097656 0.984375 15.984375 0.296875 15.46875 0.199219 C 14.746094 0.0664062 14.011719 0 13.277344 0 C 6.652344 0 1.277344 5.367188 1.277344 12 C 1.277344 18.625 6.644531 24 13.277344 24 Z M 13.277344 24 "/></g>`,
       x: `<g><path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path></g>`,
       google: `<g><path d="M9.827 17.667c-4.82 0-8.873-3.927-8.873-8.747S5.007.173 9.827.173c2.667 0 4.567 1.047 5.993 2.413l-1.687 1.687c-1.027-.96-2.413-1.707-4.307-1.707-3.52 0-6.273 2.84-6.273 6.36s2.753 6.36 6.273 6.36c2.28 0 3.587-.92 4.413-1.747.68-.68 1.132-1.668 1.3-3.008H10v-2.4h7.873c.087.428.127.935.127 1.495 0 1.793-.493 4.013-2.067 5.587-1.54 1.6-3.5 2.453-6.106 2.453zm20.806-5.627c0 3.24-2.533 5.633-5.633 5.633-3.107 0-5.633-2.387-5.633-5.633 0-3.267 2.527-5.633 5.633-5.633 3.1.006 5.633 2.373 5.633 5.633zm-2.466 0c0-2.027-1.467-3.413-3.167-3.413-1.7 0-3.167 1.387-3.167 3.413 0 2.007 1.467 3.413 3.167 3.413 1.7 0 3.167-1.406 3.167-3.413zm15.133-.007c0 3.24-2.527 5.633-5.633 5.633s-5.633-2.387-5.633-5.633c0-3.267 2.527-5.633 5.633-5.633S43.3 8.773 43.3 12.033zm-2.467 0c0-2.027-1.467-3.413-3.167-3.413S34.5 10.007 34.5 12.033c0 2.007 1.467 3.413 3.167 3.413s3.166-1.406 3.166-3.413zm14.5-5.286V16.86c0 4.16-2.453 5.867-5.353 5.867-2.733 0-4.373-1.833-4.993-3.327l2.153-.893c.387.92 1.32 2.007 2.84 2.007 1.853 0 3.007-1.153 3.007-3.307v-.813H52.9c-.553.68-1.62 1.28-2.967 1.28-2.813 0-5.267-2.453-5.267-5.613 0-3.18 2.453-5.652 5.267-5.652 1.347 0 2.413.6 2.967 1.26h.087v-.92h2.346zm-2.173 5.306c0-1.987-1.32-3.433-3.007-3.433-1.707 0-3.007 1.453-3.007 3.433 0 1.96 1.3 3.393 3.007 3.393 1.68 0 3.007-1.426 3.007-3.393zM59.807.78v16.553h-2.473V.78h2.473zm9.886 13.113l1.92 1.28c-.62.92-2.113 2.493-4.693 2.493-3.2 0-5.587-2.473-5.587-5.633 0-3.347 2.413-5.633 5.313-5.633 2.92 0 4.353 2.327 4.82 3.587l.253.64-7.534 3.113c.573 1.133 1.473 1.707 2.733 1.707s2.133-.62 2.773-1.554zm-5.906-2.026l5.033-2.093c-.28-.707-1.107-1.193-2.093-1.193-1.254 0-3.007 1.107-2.94 3.287z"></path></g>`,
-      arrow: `<g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g>`
+      arrow: `<g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g>`,
+      location: `<g><path d="M12 14.315c-2.088 0-3.787-1.698-3.787-3.786S9.913 6.74 12 6.74s3.787 1.7 3.787 3.787-1.7 3.785-3.787 3.785zm0-6.073c-1.26 0-2.287 1.026-2.287 2.287S10.74 12.814 12 12.814s2.287-1.025 2.287-2.286S13.26 8.24 12 8.24z"></path><path d="M20.692 10.69C20.692 5.9 16.792 2 12 2s-8.692 3.9-8.692 8.69c0 1.902.603 3.708 1.743 5.223l.003-.002.007.015c1.628 2.07 6.278 5.757 6.475 5.912.138.11.302.163.465.163.163 0 .327-.053.465-.162.197-.155 4.847-3.84 6.475-5.912l.007-.014.002.002c1.14-1.516 1.742-3.32 1.742-5.223zM12 20.29c-1.224-.99-4.52-3.715-5.756-5.285-.94-1.25-1.436-2.742-1.436-4.312C4.808 6.727 8.035 3.5 12 3.5s7.192 3.226 7.192 7.19c0 1.57-.497 3.062-1.436 4.313-1.236 1.57-4.532 4.294-5.756 5.285z"></path></g>`,
+      url: `<g><path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path><path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path></g>`,
+      calendar: `<g><path d="M19.708 2H4.292C3.028 2 2 3.028 2 4.292v15.416C2 20.972 3.028 22 4.292 22h15.416C20.972 22 22 20.972 22 19.708V4.292C22 3.028 20.972 2 19.708 2zm.792 17.708c0 .437-.355.792-.792.792H4.292c-.437 0-.792-.355-.792-.792V6.418c0-.437.354-.79.79-.792h15.42c.436 0 .79.355.79.79V19.71z"></path><circle cx="7.032" cy="8.75" r="1.285"></circle><circle cx="7.032" cy="13.156" r="1.285"></circle><circle cx="16.968" cy="8.75" r="1.285"></circle><circle cx="16.968" cy="13.156" r="1.285"></circle><circle cx="12" cy="8.75" r="1.285"></circle><circle cx="12" cy="13.156" r="1.285"></circle><circle cx="7.032" cy="17.486" r="1.285"></circle><circle cx="12" cy="17.486" r="1.285"></circle></g>`
     }
     return `
       <svg class="gt2-svg" viewBox="0 0 ${key == "google" ? 74 : 24} 24">
@@ -785,6 +788,130 @@
   }
 
 
+  // display standard information for blocked profile
+  function displayBlockedProfileData() {
+    let screenName = getPath().split("/")[0]
+
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: getRequestURL("https://api.twitter.com/graphql/-xfUfZsnR_zqjFd-IfrN5A/UserByScreenName", {
+        variables: {
+          screen_name: screenName,
+          withHighlightedLabel: true
+        }
+      }),
+      headers: getRequestHeaders(),
+      onload: (res) => {
+        if (res.status == 200) {
+          let profileData = JSON.parse(res.response).data.user
+
+          // get “x persons you follow follow this account” stuff
+          GM_xmlhttpRequest({
+            method: "GET",
+            url: getRequestURL("https://api.twitter.com/1.1/friends/following/list.json", {
+              include_profile_interstitial_type: 1,
+              include_blocking: 1,
+              include_blocked_by: 1,
+              include_followed_by: 1,
+              include_want_retweets: 1,
+              include_mute_edge: 1,
+              include_can_dm: 1,
+              include_can_media_tag: 1,
+              skip_status: 1,
+              cursor: -1,
+              user_id: profileData.rest_id,
+              count: 3,
+              with_total_count: true
+            }),
+            headers: getRequestHeaders(),
+            onload: (res) => {
+              if (res.status == 200) {
+                let p = profileData.legacy
+                // console.log(p);
+
+                // join date
+                let joinDate = new Date(p.created_at)
+
+                // description
+                let descr = p.description
+                .populateWithEntities(p.entities.description)
+                .replaceEmojis()
+                let matches = descr.match(/(@[0-9A-Za-z_]+)/g)
+                for (let m of matches) {
+                  descr = descr.replace(m, `<a href="/${m.slice(1)}">${m}</a>`)
+                }
+
+                $("a[href$='/header_photo'] + div > div:nth-child(2)").after(`
+                  <div class="gt2-bp-user-description">${descr}</div>
+                  <div class="gt2-bp-user-items">
+                    ${p.location != "" ? `
+                      <div class="gt2-bp-user-location">
+                        ${getSvg("location")}
+                        <span>${p.location.replaceEmojis()}</span>
+                      </div>
+                    ` : ""}
+                    ${p.url ? `
+                      <a href="${p.entities.url.urls[0].url}" class="gt2-bp-user-url">
+                        ${getSvg("url")}
+                        <span>${p.entities.url.urls[0].display_url}</span>
+                      </a>
+                    ` : ""}
+                    <div class="gt2-bp-user-joined-at">
+                      ${getSvg("calendar")}
+                      <span>
+                        ${GM_getValue("i18n_internal_rweb")
+                          .match(/\{return([^\}]*e\.joinDate[^\}]*)\}/)[1]
+                          .replace(/\"/g, "")
+                          .replace(
+                            /\+?e\.joinDate\+?/,
+                            `${joinDate.toLocaleDateString(getLang(), { month: "long" })} ${joinDate.getFullYear()}`
+                          )
+                        }
+                      </span>
+                    </div>
+                  </div>
+                `)
+
+                // followers you know
+                let fyk = JSON.parse(res.response)
+                console.log(fyk);
+
+                let fykText
+                if (fyk.total_count < 4) {
+                  fykText = locStr(`bpFollowedBy${fyk.total_count}`)
+                  .replace("$p1$", fyk.users.length > 0 ? fyk.users[0].name : "")
+                  .replace("$p2$", fyk.users.length > 1 ? fyk.users[1].name : "")
+                  .replace("$p3$", fyk.users.length > 2 ? fyk.users[2].name : "")
+                } else {
+                  fykText = locStr("bpFollowedBy4Plus")
+                  .replace("$p1$", fyk.users[0].name)
+                  .replace("$p2$", fyk.users[1].name)
+                  .replace("$nr$", fyk.total_count - 2)
+                }
+
+                let fykImg = ""
+                for (let u of fyk.users) {
+                  fykImg += `<img src="${u.profile_image_url_https}" alt="${u.name}" />`
+                }
+
+                $(".gt2-bp-user-items + div").after(`
+                  <a class="gt2-bp-followers-you-know" href="/${screenName}/followers_you_follow">
+                    ${fykImg}
+                    <span>
+                      ${fykText.replaceEmojis()}
+                    </span>
+                  </a>
+                `)
+
+              }
+            }
+          })
+        }
+      }
+    })
+  }
+
+
   // removeChild interception
   Element.prototype.removeChild = (function(fun) {
     return function(child) {
@@ -1126,6 +1253,9 @@
   })
 
 
+  // reload on unblock
+  $("body").on("click", "div[data-testid=placementTracking] div[data-testid$='-unblock']", () => window.location.reload())
+
 
   // ########################
   // #   display settings   #
@@ -1460,6 +1590,9 @@
     } else {
       $("body").removeClass("gt2-page-messages")
     }
+
+    // blocked profile page
+    waitForKeyElements("div[data-testid=placementTracking] div[data-testid$='-unblock']", displayBlockedProfileData)
 
 
     // disableAutoRefresh
