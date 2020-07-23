dcf2b90ba6959fc8e437157ce6318629ed9dbdd3
Bl4Cc4t <kuron3ko@cock.li>
2020-07-23 20:57:25 +0200

    added ability to show blocked profiles in legacy layout

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -160,7 +160,8 @@
       arrow: `<g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g>`,
       location: `<g><path d="M12 14.315c-2.088 0-3.787-1.698-3.787-3.786S9.913 6.74 12 6.74s3.787 1.7 3.787 3.787-1.7 3.785-3.787 3.785zm0-6.073c-1.26 0-2.287 1.026-2.287 2.287S10.74 12.814 12 12.814s2.287-1.025 2.287-2.286S13.26 8.24 12 8.24z"></path><path d="M20.692 10.69C20.692 5.9 16.792 2 12 2s-8.692 3.9-8.692 8.69c0 1.902.603 3.708 1.743 5.223l.003-.002.007.015c1.628 2.07 6.278 5.757 6.475 5.912.138.11.302.163.465.163.163 0 .327-.053.465-.162.197-.155 4.847-3.84 6.475-5.912l.007-.014.002.002c1.14-1.516 1.742-3.32 1.742-5.223zM12 20.29c-1.224-.99-4.52-3.715-5.756-5.285-.94-1.25-1.436-2.742-1.436-4.312C4.808 6.727 8.035 3.5 12 3.5s7.192 3.226 7.192 7.19c0 1.57-.497 3.062-1.436 4.313-1.236 1.57-4.532 4.294-5.756 5.285z"></path></g>`,
       url: `<g><path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path><path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path></g>`,
-      calendar: `<g><path d="M19.708 2H4.292C3.028 2 2 3.028 2 4.292v15.416C2 20.972 3.028 22 4.292 22h15.416C20.972 22 22 20.972 22 19.708V4.292C22 3.028 20.972 2 19.708 2zm.792 17.708c0 .437-.355.792-.792.792H4.292c-.437 0-.792-.355-.792-.792V6.418c0-.437.354-.79.79-.792h15.42c.436 0 .79.355.79.79V19.71z"></path><circle cx="7.032" cy="8.75" r="1.285"></circle><circle cx="7.032" cy="13.156" r="1.285"></circle><circle cx="16.968" cy="8.75" r="1.285"></circle><circle cx="16.968" cy="13.156" r="1.285"></circle><circle cx="12" cy="8.75" r="1.285"></circle><circle cx="12" cy="13.156" r="1.285"></circle><circle cx="7.032" cy="17.486" r="1.285"></circle><circle cx="12" cy="17.486" r="1.285"></circle></g>`
+      calendar: `<g><path d="M19.708 2H4.292C3.028 2 2 3.028 2 4.292v15.416C2 20.972 3.028 22 4.292 22h15.416C20.972 22 22 20.972 22 19.708V4.292C22 3.028 20.972 2 19.708 2zm.792 17.708c0 .437-.355.792-.792.792H4.292c-.437 0-.792-.355-.792-.792V6.418c0-.437.354-.79.79-.792h15.42c.436 0 .79.355.79.79V19.71z"></path><circle cx="7.032" cy="8.75" r="1.285"></circle><circle cx="7.032" cy="13.156" r="1.285"></circle><circle cx="16.968" cy="8.75" r="1.285"></circle><circle cx="16.968" cy="13.156" r="1.285"></circle><circle cx="12" cy="8.75" r="1.285"></circle><circle cx="12" cy="13.156" r="1.285"></circle><circle cx="7.032" cy="17.486" r="1.285"></circle><circle cx="12" cy="17.486" r="1.285"></circle></g>`,
+      balloon: `<g><path d="M7.75 11.083c-.414 0-.75-.336-.75-.75C7 7.393 9.243 5 12 5c.414 0 .75.336.75.75s-.336.75-.75.75c-1.93 0-3.5 1.72-3.5 3.833 0 .414-.336.75-.75.75z"></path><path d="M20.75 10.333c0-5.01-3.925-9.083-8.75-9.083s-8.75 4.074-8.75 9.083c0 4.605 3.32 8.412 7.605 8.997l-1.7 1.83c-.137.145-.173.357-.093.54.08.182.26.3.46.3h4.957c.198 0 .378-.118.457-.3.08-.183.044-.395-.092-.54l-1.7-1.83c4.285-.585 7.605-4.392 7.605-8.997zM12 17.917c-3.998 0-7.25-3.402-7.25-7.584S8.002 2.75 12 2.75s7.25 3.4 7.25 7.583-3.252 7.584-7.25 7.584z"></path></g>`,
     }
     return `
       <svg class="gt2-svg" viewBox="0 0 ${key == "google" ? 74 : 24} 24">
@@ -685,7 +686,7 @@
 
       // information (constant)
       const i = {
-        avatarUrl:    `${$("a[href$='/photo'] img").attr("src").replace(/_(bigger|normal|\d+x\d+)/, "")}`,
+        avatarUrl:    $("a[href$='/photo'] img").attr("src").replace(/_(bigger|normal|\d*x\d+)/, "_400x400"),
         screenName:   $profile.find("> div:nth-child(2) > div > div > div:nth-child(2) span").text().slice(1),
         nameHTML:     $profile.find("> div:nth-child(2) > div > div > div:nth-child(1) > div > span:nth-child(1)").html(),
         joinDateHTML: $profile.find("div[data-testid=UserProfileHeader_Items] > span:last-child").html(),
@@ -719,14 +720,14 @@
                 <div>${i.followers.humanizeShort()}</div>
               </a>
               <!--
-              <a href="/${i.screenName}/lists" title="${i.following.humanize()}">
-                <div>${getLocStr("navLists")}</div>
-                <div>${i.followers.humanizeShort()}</div>
-              </a>
-              <a href="/${i.screenName}/moments" title="${i.following.humanize()}">
-                <div>${getLocStr("statsMoments")}</div>
-                <div>${i.followers.humanizeShort()}</div>
-              </a>
+                <a href="/${i.screenName}/lists" title="${i.following.humanize()}">
+                  <div>${getLocStr("navLists")}</div>
+                  <div>${i.followers.humanizeShort()}</div>
+                </a>
+                <a href="/${i.screenName}/moments" title="${i.following.humanize()}">
+                  <div>${getLocStr("statsMoments")}</div>
+                  <div>${i.followers.humanizeShort()}</div>
+                </a>
               -->
             </div>
             <div class="gt2-legacy-profile-nav-right"></div>
@@ -734,6 +735,7 @@
         `)
       }
 
+      // add like and tweet count
       GM_xmlhttpRequest({
         method: "GET",
         url: getRequestURL("https://api.twitter.com/graphql/-xfUfZsnR_zqjFd-IfrN5A/UserByScreenName", {
@@ -777,16 +779,20 @@
             ${e.$location.length    ? `<div class="gt2-legacy-profile-item">${e.$location.html()}</div>`                    : ""}
             ${e.$url.length         ? `<div class="gt2-legacy-profile-item">${e.$url.prop("outerHTML")}</div>`              : ""}
             ${e.$birthday.length && e.$birthday.find("path[d^='M7.75']").length ? `<div class="gt2-legacy-profile-item">${e.$birthday.html()}</div>` : ""}
-            <div class="gt2-legacy-profile-item">${i.joinDateHTML}</div>
+            ${i.joinDateHTML        ? `<div class="gt2-legacy-profile-item">${i.joinDateHTML}</div>`                        : ""}
             ${e.$fyk.length         ? `<div class="gt2-legacy-profile-fyk">${e.$fyk.prop("outerHTML")}</div>`               : ""}
           `)
 
           // followers you know
+          GM_setValue("hasRun_InsertFYK", false)
           waitForKeyElements("a[href$='/followers_you_follow']", e => {
-            $(".gt2-legacy-profile-fyk").html($(e).prop("outerHTML"))
+            if (!GM_getValue("hasRun_InsertFYK")) {
+              $(".gt2-legacy-profile-fyk").html($(e).prop("outerHTML"))
+              GM_setValue("hasRun_InsertFYK", true)
+            }
           })
         }
-})
+      })
 
       // buttons
       if (!$(".gt2-legacy-profile-nav-right > div").length) {
@@ -897,6 +903,62 @@
   }
 
 
+  function getFollowersYouKnowHTML(screenName, profileID, callback) {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: getRequestURL("https://api.twitter.com/1.1/friends/following/list.json", {
+        include_profile_interstitial_type: 1,
+        include_blocking: 1,
+        include_blocked_by: 1,
+        include_followed_by: 1,
+        include_want_retweets: 1,
+        include_mute_edge: 1,
+        include_can_dm: 1,
+        include_can_media_tag: 1,
+        skip_status: 1,
+        cursor: -1,
+        user_id: profileID,
+        count: 3,
+        with_total_count: true
+      }),
+      headers: getRequestHeaders(),
+      onload: (res) => {
+        if (res.status == 200) {
+
+          // followers you know
+          let fyk = JSON.parse(res.response)
+
+          let fykText
+          if (fyk.total_count < 4) {
+            fykText = getLocStr(`followedBy${fyk.total_count}`)
+            .replace("$p1$", fyk.users.length > 0 ? fyk.users[0].name : "")
+            .replace("$p2$", fyk.users.length > 1 ? fyk.users[1].name : "")
+            .replace("$p3$", fyk.users.length > 2 ? fyk.users[2].name : "")
+          } else {
+            fykText = getLocStr("followedBy4Plus")
+            .replace("$p1$", fyk.users[0].name)
+            .replace("$p2$", fyk.users[1].name)
+            .replace("$nr$", fyk.total_count - 2)
+          }
+
+          let fykImg = ""
+          for (let u of fyk.users) {
+            fykImg += `<img src="${u.profile_image_url_https}" alt="${u.name}" />`
+          }
+
+          callback(`
+            <a class="gt2-blocked-profile-followers-you-know" href="/${screenName}/followers_you_follow">
+              ${fykImg}
+              <span>
+                ${fykText.replaceEmojis()}
+              </span>
+            </a>
+          `)
+        }
+      }
+    })
+  }
+
   // display standard information for blocked profile
   function displayBlockedProfileData() {
     let screenName = getPath().split("/")[0]
@@ -915,101 +977,97 @@
           let profileData = JSON.parse(res.response).data.user
 
           // get “x persons you follow follow this account” stuff
-          GM_xmlhttpRequest({
-            method: "GET",
-            url: getRequestURL("https://api.twitter.com/1.1/friends/following/list.json", {
-              include_profile_interstitial_type: 1,
-              include_blocking: 1,
-              include_blocked_by: 1,
-              include_followed_by: 1,
-              include_want_retweets: 1,
-              include_mute_edge: 1,
-              include_can_dm: 1,
-              include_can_media_tag: 1,
-              skip_status: 1,
-              cursor: -1,
-              user_id: profileData.rest_id,
-              count: 3,
-              with_total_count: true
-            }),
-            headers: getRequestHeaders(),
-            onload: (res) => {
-              if (res.status == 200) {
-                let p = profileData.legacy
-                // console.log(p);
+          getFollowersYouKnowHTML(screenName, profileData.rest_id, fykHTML => {
 
-                // join date
-                let joinDate = new Date(p.created_at)
+            let pleg = profileData.legacy
 
-                // description
-                let descr = p.description
-                .populateWithEntities(p.entities.description)
-                .replaceEmojis()
-                let matches = descr.match(/(@[0-9A-Za-z_]+)/g) || []
-                for (let m of matches) {
-                  descr = descr.replace(m, `<a href="/${m.slice(1)}">${m}</a>`)
-                }
+            // join date
+            let joinDate = new Date(pleg.created_at)
 
-                $("a[href$='/header_photo'] + div > div:nth-child(2)").after(`
-                  <div class="gt2-bp-user-description">${descr}</div>
-                  <div class="gt2-bp-user-items">
-                    ${p.location != "" ? `
-                      <div class="gt2-bp-user-location">
-                        ${getSvg("location")}
-                        <span>${p.location.replaceEmojis()}</span>
-                      </div>
-                    ` : ""}
-                    ${p.url ? `
-                      <a href="${p.entities.url.urls[0].url}" class="gt2-bp-user-url">
-                        ${getSvg("url")}
-                        <span>${p.entities.url.urls[0].display_url}</span>
-                      </a>
-                    ` : ""}
-                    <div class="gt2-bp-user-joined-at">
-                      ${getSvg("calendar")}
-                      <span>
-                        ${
-                          getLocStr("joinDate")
-                          .replace("$date$", `${joinDate.toLocaleDateString(getLang(), { month: "long" })} ${joinDate.getFullYear}`)
-                        }
-                      </span>
-                    </div>
-                  </div>
-                `)
+            let p = {
+              description:  pleg.description
+                            .populateWithEntities(pleg.entities.description)
+                            .replaceEmojis(),
+              location:     pleg.location != "" ? `
+                              <div class="gt2-blocked-profile-location">
+                                ${getSvg("location")}
+                                <span>${pleg.location.replaceEmojis()}</span>
+                              </div>` : null,
+              url:          pleg.url ? `
+                              <a href="${pleg.entities.url.urls[0].url}" class="gt2-blocked-profile-url">
+                                ${getSvg("url")}
+                                <span>${pleg.entities.url.urls[0].display_url}</span>
+                              </a>` : null,
+              joinDate:     `<div class="gt2-blocked-profile-joined-at">
+                              ${getSvg("calendar")}
+                              <span>
+                                ${
+                                  getLocStr("joinDate")
+                                  .replace("$date$", joinDate.toLocaleDateString(getLang(), { month: "long", year: "numeric" }))
+                                }
+                              </span>
+                            </div>`,
+              birthday:     profileData.legacy_extended_profile && profileData.legacy_extended_profile.birthdate ? (() => {
+                              let bd = profileData.legacy_extended_profile.birthdate
+                              let bdText
+                              let date = new Date(Date.UTC(bd.year || 1970, bd.month || 1, bd.day || 1))
+                              if (bd.year && !bd.month && !bd.day) {
+                                bdText = getLocStr("bornYear").replace("$year$", date.toLocaleDateString(getLang(), { year: "numeric"}))
+                              } else {
+                                let opt = {}
+                                if (bd.year)  opt.year = "numeric"
+                                if (bd.month) opt.month = "long"
+                                if (bd.day)   opt.day = "numeric"
+                                bdText = getLocStr("bornDate").replace("$date$", date.toLocaleDateString(getLang(), opt))
+                              }
 
-                // followers you know
-                let fyk = JSON.parse(res.response)
+                              return `
+                                <div class="gt2-blocked-profile-birthday">
+                                ${getSvg("balloon")}
+                                  <span>${bdText}</span>
+                                </div>`
+                            })() : null
 
-                let fykText
-                if (fyk.total_count < 4) {
-                  fykText = getLocStr(`followedBy${fyk.total_count}`)
-                  .replace("$p1$", fyk.users.length > 0 ? fyk.users[0].name : "")
-                  .replace("$p2$", fyk.users.length > 1 ? fyk.users[1].name : "")
-                  .replace("$p3$", fyk.users.length > 2 ? fyk.users[2].name : "")
-                } else {
-                  fykText = getLocStr("followedBy4Plus")
-                  .replace("$p1$", fyk.users[0].name)
-                  .replace("$p2$", fyk.users[1].name)
-                  .replace("$nr$", fyk.total_count - 2)
-                }
-
-                let fykImg = ""
-                for (let u of fyk.users) {
-                  fykImg += `<img src="${u.profile_image_url_https}" alt="${u.name}" />`
-                }
-
-                $(".gt2-bp-user-items + div").after(`
-                  <a class="gt2-bp-followers-you-know" href="/${screenName}/followers_you_follow">
-                    ${fykImg}
-                    <span>
-                      ${fykText.replaceEmojis()}
-                    </span>
-                  </a>
-                `)
-
-              }
             }
+
+            // description: add links for mentioned users
+            for (let m of p.description.match(/(@[0-9A-Za-z_]+)/g) || []) {
+              p.description = p.description.replace(m, `<a href="/${m.slice(1)}">${m}</a>`)
+            }
+
+            // add profile info
+            $("a[href$='/header_photo'] + div > div:nth-child(2)").after(`
+              <div class="gt2-blocked-profile-description">${p.description}</div>
+              <div class="gt2-blocked-profile-items">
+                ${p.location ? p.location : ""}
+                ${p.url      ? p.url      : ""}
+                ${p.birthday ? p.birthday : ""}
+                ${p.joinDate}
+              </div>
+            `)
+
+
+            $(".gt2-blocked-profile-items + div").after(fykHTML)
+
+
+
+            // add legacy sidebar profile information
+            waitForKeyElements(".gt2-legacy-profile-name", () => {
+              if (!$(".gt2-legacy-profile-info .gt2-legacy-profile-fyk").length) {
+                $(".gt2-legacy-profile-info").append(`
+                  ${p.description ? `<div class="gt2-legacy-profile-description">${p.description}</div>` : ""}
+                  ${p.location    ? `<div class="gt2-legacy-profile-item">${p.location}</div>`           : ""}
+                  ${p.url         ? `<div class="gt2-legacy-profile-item">${p.url}</div>`                : ""}
+                  ${p.birthday    ? `<div class="gt2-legacy-profile-item">${p.birthday}</div>`           : ""}
+                  <div class="gt2-legacy-profile-item">${p.joinDate}</div>
+                  <div class="gt2-legacy-profile-fyk">${fykHTML}</div>
+                `)
+              }
+            })
+
+
           })
+
         }
       }
     })
@@ -1359,10 +1417,6 @@
     }
     $(this).parents(".gt2-sidebar-notice").remove()
   })
-
-
-  // reload on unblock
-  $("body").on("click", "div[data-testid=placementTracking] div[data-testid$='-unblock']", () => window.location.reload())
 
 
 
