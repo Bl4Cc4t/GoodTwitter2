35f11a5dc2e38d150fb29993f4bac4fb3b7f35ae
Bl4Cc4t <kuron3ko@cock.li>
2020-12-23 20:05:47 +0100

    v0.0.28 compilations/adjustments

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GoodTwitter 2 - Electric Boogaloo
-// @version       0.0.27
+// @version       0.0.28
 // @description   A try to make Twitter look good again
 // @author        schwarzkatz
 // @license       MIT
