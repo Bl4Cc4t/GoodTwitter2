549a4d0a9511b8bb9ed75b2635f7fb62974d50c5
josephwhite <damasta21th@aol.com>
2021-01-08 01:41:56 -0500

    formatting fixes

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -372,7 +372,7 @@
     hideWhoToFollow:          false,
     hideTranslateTweetButton: false,
     hideMessageBox:           true,
-	enableQuickBlock:         true,
+    enableQuickBlock:         true,
     legacyProfile:            false,
     showNsfwMessageMedia:     false,
     expandTcoShortlinks:      true
