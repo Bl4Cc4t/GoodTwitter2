0ea3940ba6b32e20b5c2a2372f38311a035ee1af
Bl4Cc4t <kuron3ko@cock.li>
2022-08-27 17:19:12 +0200

    feat: allow multiple arguments in logger

--- src/ts/util/logger.ts
+++ src/ts/util/logger.ts
@@ -9,17 +9,19 @@ const LOG_PREFIX = "[GT2]"
 
 class Logger {
   debug(...msg: any) {
-    console.debug(LOG_PREFIX, msg)
+    console.debug(LOG_PREFIX, ...msg)
   }
+
   info(...msg: any) {
-    console.info(LOG_PREFIX, msg)
+    console.info(LOG_PREFIX, ...msg)
   }
+
   warn(...msg: any) {
-    console.warn(LOG_PREFIX, msg)
+    console.warn(LOG_PREFIX, ...msg)
   }
 
   error(...msg: any) {
-    console.error(LOG_PREFIX, msg)
+    console.error(LOG_PREFIX, ...msg)
   }
 }
 
