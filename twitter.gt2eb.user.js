57eb5b6c7214843182d892931f045d7f6da0aaaa
Bl4Cc4t <kuron3ko@cock.li>
2022-09-18 19:05:54 +0200

    release: v0.0.40.4

--- twitter.gt2eb.user.js
+++ twitter.gt2eb.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          GoodTwitter 2 - Electric Boogaloo
-// @version       0.0.40.3
+// @version       0.0.40.4
 // @description   A try to make Twitter look good again.
 // @author        schwarzkatz
 // @license       MIT
