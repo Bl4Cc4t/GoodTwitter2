21a6b199b5a411dfd7bce62cc475d20bff7c0a01
Bl4Cc4t <kuron3ko@cock.li>
2022-09-25 20:46:57 +0200

    refactor: better variable names for override functions

--- src/ts/util/overrides.ts
+++ src/ts/util/overrides.ts
@@ -1,41 +1,40 @@
-import { TwitterApi } from "../types"
 import { onLocationChange } from "./location"
 import { saveTweetDetailData } from "./tweet"
 
 
 export function overrideFunctions() {
   // remove whitespace when inserting HTML
-  const _insertAdjacentHTML = Element.prototype.insertAdjacentHTML
+  const Element_insertAdjacentHTML = Element.prototype.insertAdjacentHTML
   Element.prototype.insertAdjacentHTML = function(position, text) {
-    _insertAdjacentHTML.call(this, position, text.trim())
+    Element_insertAdjacentHTML.call(this, position, text.trim())
   }
 
   // remove "t" search parameter (probably used for tracking?)
   // https://twitter.com/Outrojules/status/1543220843995619328?s=20&t=fCFEatQ_iAtlyiHQCWCxoQ
-  const _selectNodeContents = Range.prototype.selectNodeContents
+  const Range_selectNodeContents = Range.prototype.selectNodeContents
   Range.prototype.selectNodeContents = function(node) {
     node.textContent = node.textContent.replace(/&t=.*$/, "")
-    _selectNodeContents.call(this, node)
+    Range_selectNodeContents.call(this, node)
   }
 
-  const _removeChild = Node.prototype.removeChild
+  const Node_removeChild = Node.prototype.removeChild
   Node.prototype.removeChild = function<T extends Node>(child: T): T {
     // prevent removal of untranslated tweet texts in timeline
     if (child instanceof HTMLElement && child.dataset.testid == "tweetText") {
       return child
     }
-    return _removeChild.call(this, child)
+    return Node_removeChild.call(this, child)
   }
 
-  const _push = History.prototype.pushState
+  const History_push = History.prototype.pushState
   History.prototype.pushState = function() {
-    _push.apply(this, arguments)
+    History_push.apply(this, arguments)
     onLocationChange("push")
   }
 
-  const _replace = History.prototype.replaceState
+  const History_replace = History.prototype.replaceState
   History.prototype.replaceState = function() {
-    _replace.apply(this, arguments)
+    History_replace.apply(this, arguments)
     onLocationChange("replace")
   }
 
