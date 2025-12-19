8d73f304e238867d0e57383ebfe34df19a4009ab
Bl4Cc4t <kuron3ko@cock.li>
2025-12-19 21:50:43 +0100

    fix: forceLatest

--- src/ts/util/timeline.ts
+++ src/ts/util/timeline.ts
@@ -16,38 +16,7 @@ export function enableLatestTweets(): void {
     if (!Settings.get("forceLatest"))
         return
 
-    let sparkOptSelector = `[d*="M2 4c1.66 0 3-1.34 3-3h1c0"]`
-    let dropdownSelector = "#layers [data-testid=Dropdown]"
-
-    waitForElements(sparkOptSelector, toggleIcon => {
-        const toggleDropdown = toggleIcon.closest<HTMLElement>("[aria-haspopup]");
-        if (toggleDropdown == null) {
-            _logger.error("spark button not found")
-            return
-        }
-
-        document.body.classList.add("gt2-hide-spark-opt")
-        toggleDropdown.click()
-        _logger.debug("toggled spark dropdown menu")
-
-        waitForElements(`${dropdownSelector} a[href="/settings/content_preferences"]`, e => {
-            let dropdown = e.closest(dropdownSelector)
-
-            // switch to latest
-            if (dropdown.querySelectorAll(":scope > :nth-child(1) path").length == 3) {
-                const enableLatest = dropdown.querySelector<HTMLElement>(":scope > :nth-child(2)");
-                enableLatest.click()
-                _logger.info("switched home timeline to latest tweets")
-            }
-
-            // hide dropdown again
-            else {
-                toggleDropdown.click()
-                _logger.debug("timeline already on latest tweets mode, nothing to do")
-            }
-            document.body.classList.remove("gt2-hide-spark-opt")
-        })
-    })
+    waitForElements(`[data-testid=primaryColumn] > :first-child > :first-child nav [role=presentation]:nth-child(2) > [aria-selected=false]`, header => header.click())
 }
 
 
